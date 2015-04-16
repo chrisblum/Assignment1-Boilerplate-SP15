1eb7ba29c6254d857ea1d65e02025ae3e4af6b11
Chris Blum <cblum@ucsd.edu>
2015-04-15 21:14:52 -0700

    sadf

--- app.js
+++ app.js
@@ -173,13 +173,12 @@ app.get('/login', function(req, res){
 app.get('/account', ensureAuthenticated, function(req, res){
 
   graph.setAccessToken(req.session.access_token);
-  graph.get('/me', function(err, data) {
+  var boom = graph.get('/me', function(err, data) {
     console.log(data);
-    res.render('account', data);
   });
 
 
-  // res.render('account', data);
+  res.render('account', boom);
 });
 
 
