651aa55d4d578763bdf6386862ed3b98c6ddfee7
Chris Blum <cblum@ucsd.edu>
2015-04-17 17:56:30 -0700

    Sasd

--- models.js
+++ models.js
@@ -2,19 +2,19 @@ var mongoose = require('mongoose');
 var findOrCreate = require('mongoose-findorcreate');
 
 
-// var userSchema = mongoose.Schema({
-// 	"name" : { type: String },
-// 	"id" : { type: String },
-// 	"access_token" : { type: String }
-// });
-
 var userSchema = mongoose.Schema({
 	"name" : { type: String },
-	"fb_id" : { type: String },
-	"ig_id" : { type: String },
-	"fb_access_token" : { type: String },
-	"ig_access_token" : { type: String }
+	"id" : { type: String },
+	"access_token" : { type: String }
 });
+
+// var userSchema = mongoose.Schema({
+// 	"name" : { type: String },
+// 	"fb_id" : { type: String },
+// 	"ig_id" : { type: String },
+// 	"fb_access_token" : { type: String },
+// 	"ig_access_token" : { type: String }
+// });
 
 
 userSchema.plugin(findOrCreate);
